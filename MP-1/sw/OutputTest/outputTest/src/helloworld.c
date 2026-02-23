433355cecb3ee774cbd174e405ece273459693bd
Hemmestad <cihem@iastate.edu>
2026-02-23 00:52:24 -0600

    Everything except filter mode works

--- MP-1/sw/OutputTest/outputTest/src/helloworld.c
+++ MP-1/sw/OutputTest/outputTest/src/helloworld.c
@@ -75,6 +75,7 @@ static u32 saved_frames[MAX_SAVED_FRAMES][CAPTURE_COUNT];
 static u32 saved_count = 0U;
 static int max_frames_reported = 0;
 static u32 record_index = 0U;
+static u32 play_index = 0U;
 
 static void save_register_snapshot(u32 frame_idx)
 {
@@ -86,32 +87,30 @@ static void save_register_snapshot(u32 frame_idx)
     saved_frames[frame_idx][5] = Xil_In32(PPM_BASE + REG15);
 }
 
-static void load_dummy_frame(void)
+static void load_most_recent_frame(void)
 {
-    /* Dummy software-relay frame for generate FSM (regs 4..9). */
-    Xil_Out32(PPM_BASE + REG4, 100000U);
-    Xil_Out32(PPM_BASE + REG5, 100000U);
-    Xil_Out32(PPM_BASE + REG6, 100000U);
-    Xil_Out32(PPM_BASE + REG7, 100000U);
-    Xil_Out32(PPM_BASE + REG8, 100000U);
-    Xil_Out32(PPM_BASE + REG9, 100000U);
-
-    xil_printf("Loaded dummy frame: r4=%u r5=%u r6=%u r7=%u r8=%u r9=%u\r\n",
-               (u32)Xil_In32(PPM_BASE + REG4),
-               (u32)Xil_In32(PPM_BASE + REG5),
-               (u32)Xil_In32(PPM_BASE + REG6),
-               (u32)Xil_In32(PPM_BASE + REG7),
-               (u32)Xil_In32(PPM_BASE + REG8),
-               (u32)Xil_In32(PPM_BASE + REG9));
+    /* Copy the latest captured frame (r10..r15) into generator regs (r4..r9). */
+    Xil_Out32(PPM_BASE + REG4, Xil_In32(PPM_BASE + REG10));
+    Xil_Out32(PPM_BASE + REG5, Xil_In32(PPM_BASE + REG11));
+    Xil_Out32(PPM_BASE + REG6, Xil_In32(PPM_BASE + REG12));
+    Xil_Out32(PPM_BASE + REG7, Xil_In32(PPM_BASE + REG13));
+    Xil_Out32(PPM_BASE + REG8, Xil_In32(PPM_BASE + REG14));
+    Xil_Out32(PPM_BASE + REG9, Xil_In32(PPM_BASE + REG15));
 }
 
 /* SW0 comes from AXI GPIO channel 1, bit 0. */
 #define SW0_MASK 0x1U
+/* SW1 enables software debug UART prints. */
+#define SW1_MASK 0x2U
 /* SW2 is bit 2 on the same switch GPIO input. */
 #define SW2_MASK 0x4U
-/* Button bit mapping: bit4=BTNU, bit1=BTND, bit0=BTNC */
+/* SW3 is bit 3 on the same switch GPIO input. */
+#define SW3_MASK 0x8U
+/* Button bit mapping: bit4=BTNU, bit3=BTNR, bit2=BTNL, bit1=BTND, bit0=BTNC */
 #define BTNC_MASK 0x1U
 #define BTND_MASK 0x2U
+#define BTNL_MASK 0x4U
+#define BTNR_MASK 0x8U
 #define BTNU_MASK 0x10U
 
 #if defined(XPAR_AXI_GPIO_1_BASEADDR)
@@ -153,9 +152,13 @@ static inline u32 access_buttons(void)
 int main()
 {
     int last_sw0 = -1;
+    int last_sw1 = -1;
     int last_sw2 = -1;
+    int last_sw3 = -1;
     int prev_btnc = 0;
     int prev_btnd = 0;
+    int prev_btnl = 0;
+    int prev_btnr = 0;
     int prev_btnu = 0;
     u32 prev_reg1 = Xil_In32(PPM_BASE + REG1);
 
@@ -167,12 +170,18 @@ int main()
         u32 sw_data = access_switches();
         u32 btn_data = access_buttons();
         int sw0 = ((sw_data & SW0_MASK) != 0U) ? 1 : 0;
+        int sw1 = ((sw_data & SW1_MASK) != 0U) ? 1 : 0;
         int sw2 = ((sw_data & SW2_MASK) != 0U) ? 1 : 0;
+        int sw3 = ((sw_data & SW3_MASK) != 0U) ? 1 : 0;
         int btnc = ((btn_data & BTNC_MASK) != 0U) ? 1 : 0;
         int btnd = ((btn_data & BTND_MASK) != 0U) ? 1 : 0;
+        int btnl = ((btn_data & BTNL_MASK) != 0U) ? 1 : 0;
+        int btnr = ((btn_data & BTNR_MASK) != 0U) ? 1 : 0;
         int btnu = ((btn_data & BTNU_MASK) != 0U) ? 1 : 0;
         int btnc_rise = (btnc == 1 && prev_btnc == 0);
         int btnd_rise = (btnd == 1 && prev_btnd == 0);
+        int btnl_rise = (btnl == 1 && prev_btnl == 0);
+        int btnr_rise = (btnr == 1 && prev_btnr == 0);
         int btnu_rise = (btnu == 1 && prev_btnu == 0);
 
         if (btnc_rise) {
@@ -187,11 +196,25 @@ int main()
                            (u32)Xil_In32(PPM_BASE + REG0));
             } else {
                 Xil_Out32(PPM_BASE + REG0, 1U);  /* software relay */
-                load_dummy_frame();
+                load_most_recent_frame();
+
                 xil_printf("SW0=1 -> software relay (REG0=%u)\r\n",
                            (u32)Xil_In32(PPM_BASE + REG0));
             }
             last_sw0 = sw0;
+        }
+
+        if (sw0 == 1) {
+            load_most_recent_frame();
+        }
+
+        if (sw1 != last_sw1) {
+            if (sw1 == 1) {
+                xil_printf("SW1=1 -> debug mode enabled\r\n");
+            } else {
+                xil_printf("SW1=0 -> debug mode disabled\r\n");
+            }
+            last_sw1 = sw1;
         }
 
         if (sw2 != last_sw2) {
@@ -202,6 +225,15 @@ int main()
                 xil_printf("SW2=0 -> record mode disabled\r\n");
             }
             last_sw2 = sw2;
+        }
+
+        if (sw3 != last_sw3) {
+            if (sw3 == 1) {
+                xil_printf("SW3=1 -> play mode enabled\r\n");
+            } else {
+                xil_printf("SW3=0 -> play mode disabled\r\n");
+            }
+            last_sw3 = sw3;
         }
 
         if (sw2 == 1) {
@@ -252,9 +284,58 @@ int main()
             }
         }
 
+        if (sw3 == 1) {
+            if (btnl == 1) {
+                if (play_index > 0U) {
+                    play_index--;
+                }
+                xil_printf("BTNL held -> play index %u\r\n", play_index);
+            }
+
+            if (btnr == 1) {
+                if (saved_count == 0U) {
+                    xil_printf("BTNR held -> no saved frames to play\r\n");
+                } else {
+                    Xil_Out32(PPM_BASE + REG0, 1U); /* force software relay mode for playback */
+                    Xil_Out32(PPM_BASE + REG4, saved_frames[play_index][0]);
+                    Xil_Out32(PPM_BASE + REG5, saved_frames[play_index][1]);
+                    Xil_Out32(PPM_BASE + REG6, saved_frames[play_index][2]);
+                    Xil_Out32(PPM_BASE + REG7, saved_frames[play_index][3]);
+                    Xil_Out32(PPM_BASE + REG8, saved_frames[play_index][4]);
+                    Xil_Out32(PPM_BASE + REG9, saved_frames[play_index][5]);
+
+                    xil_printf("BTNR played frame %u: r4=%u r5=%u r6=%u r7=%u r8=%u r9=%u\r\n",
+                               play_index,
+                               saved_frames[play_index][0],
+                               saved_frames[play_index][1],
+                               saved_frames[play_index][2],
+                               saved_frames[play_index][3],
+                               saved_frames[play_index][4],
+                               saved_frames[play_index][5]);
+
+                    if (play_index + 1U < saved_count) {
+                        play_index++;
+                    }
+                }
+            }
+        }
+
+        if (sw1 == 1) {
+            xil_printf("DBG: r10=%u r11=%u r12=%u r13=%u r14=%u r15=%u\r\n",
+                       (u32)Xil_In32(PPM_BASE + REG10),
+                       (u32)Xil_In32(PPM_BASE + REG11),
+                       (u32)Xil_In32(PPM_BASE + REG12),
+                       (u32)Xil_In32(PPM_BASE + REG13),
+                       (u32)Xil_In32(PPM_BASE + REG14),
+                       (u32)Xil_In32(PPM_BASE + REG15));
+        }
+
         prev_btnc = btnc;
         prev_btnd = btnd;
+        prev_btnl = btnl;
+        prev_btnr = btnr;
         prev_btnu = btnu;
+        usleep(100000);
     }
 
     cleanup_platform();
